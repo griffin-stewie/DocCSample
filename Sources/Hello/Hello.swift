3f87ad9ad4167e03d1525cfaa0af7bf26c114ce0
griffin-stewie <panterathefamilyguy@gmail.com>
2021-06-12 14:42:55 +0900

    init

---
+++ Sources/Hello/Hello.swift
@@ -0,0 +1,10 @@
+/// Hello struct
+public struct Hello {
+    /// Greeting
+    public let text = "Hello, World!"
+    
+    /// Prints ``text`` property
+    public func print() {
+        Swift.print(text)
+    }
+}
